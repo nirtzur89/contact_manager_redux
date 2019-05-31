f6c6d65d0bcc13ef2b19bdcc2e953abb8ad684aa
nirtzur <nirtzur89@gmail.com>
2019-05-31 15:59:48 +0200

    redux setup

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import Contact from './Contact';
+import React, { Component } from "react";
+import Contact from "./Contact";
 
 class Contacts extends Component {
   state = {
     contacts: [
       {
         id: 1,
-        name: 'John Doe',
-        email: 'john@gmail.com',
-        phone: '555-555-5555'
+        name: "Nir Tzur",
+        email: "john@gmail.com",
+        phone: "555-555-5555"
       },
       {
         id: 2,
-        name: 'Karen Williams',
-        email: 'karen@gmail.com',
-        phone: '444-444-4444'
+        name: "Karen Williams",
+        email: "karen@gmail.com",
+        phone: "444-444-4444"
       },
       {
         id: 3,
-        name: 'Henry Johnson',
-        email: 'henry@gmail.com',
-        phone: '333-333-333'
+        name: "Henry Johnson",
+        email: "henry@gmail.com",
+        phone: "333-333-333"
       }
     ]
   };
